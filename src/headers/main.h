20e163da39a028b25ec5ead79e463b415f193aef
Ilya Posmygaev <ilya.posmis@gmail.com>
2023-12-07 00:42:57 +0300

    Init base files

---
+++ src/headers/main.h
@@ -0,0 +1,4 @@
+#pragma once
+
+#include <iostream>
+#include "vector_3D.h"
---
+++ src/sources/main.cpp
@@ -0,0 +1,8 @@
+#include "../headers/main.h"
+
+using namespace std;
+
+int main(void){
+    cout << "Hello World!" << endl;
+    return 0;
+}
